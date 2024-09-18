7455e2587f54058214035bcdb54404f7d2bd1845
anjali2006-coder <anjalibahuguna678@gmail.com>
2024-09-18 19:16:48 +0530

    my first loop program

---
+++ program1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a,N;
+    printf("Enter the any value =");
+    scanf("%d",&N);
+    printf("The value of N=%d",N);
+    for(a=1;a<=N;a++){
+        printf("\n%d",a);
+    }
+}
